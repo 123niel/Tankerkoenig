67c831c4139aa665861b24de93a1fa54ae8ddc8e
Niel Rohling <niel.rohling@gmail.com>
2022-06-03 12:49:24 +0200

    important changes

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/fgrosse/graphigo"
-	"log"
 	"time"
 
 	"github.com/segmentio/kafka-go"
@@ -42,12 +41,10 @@ func main() {
 	done := make(chan bool)
 
 	for i := 0; i <= 9; i++ {
-		go func() {
-			priceData := readPriceDataFromPartition(i)
-			aggregations := aggregate(priceData)
-			sendData(aggregations, graphiteClient)
+		go func(partition int) {
+			readKafka(partition, graphiteClient)
 			done <- true
-		}()
+		}(i)
 	}
 
 	for i := 0; i <= 9; i++ {
@@ -55,95 +52,66 @@ func main() {
 	}
 }
 
-func readPriceDataFromPartition(partition int) []PriceData {
-	conn := openConnection(partition)
+func readKafka(partition int, client *graphigo.Client) {
+	r := getReader(partition)
 
-	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-	batch := conn.ReadBatch(1e3, 1e6)
-
-	b := make([]byte, 10e3) // 10KB max per message
-
-	var dataSlice []PriceData
+	dataSlice := make([]PriceData, 1)
+	var currentHour time.Time
 
 	for {
-		n, err := batch.Read(b)
+		m, err := r.ReadMessage(context.Background())
+
 		if err != nil {
 			break
 		}
-		var data PriceData
-		err = json.Unmarshal(b[:n], &data)
-		dataSlice = append(dataSlice, data)
 
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-	}
+		data := parseJSON(m.Value)
 
-	if err := batch.Close(); err != nil {
-		log.Fatal("failed to close batch:", err)
-	}
+		if data.Date.Hour() == currentHour.Hour() {
 
-	if err := conn.Close(); err != nil {
-		log.Fatal("failed to close connection:", err)
-	}
-
-	return dataSlice
-}
-
-func aggregate(data []PriceData) []Aggregation {
-	fmt.Sprintf("Aggregating for region %d", data[0].PostCode)
-	lastDate := time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
-	pE5Sum := float64(0)
-	PE10Sum := float64(0)
-	pDieselSum := float64(0)
-	count := float64(0)
-	var aggregations []Aggregation
-
-	for _, element := range data {
-		if element.Date.Hour() == lastDate.Hour() {
-			pE5Sum += element.PE5
-			PE10Sum += element.PE10
-			pDieselSum += element.PDiesel
-			count++
 		} else {
-			tempDate := time.Date(lastDate.Year(), lastDate.Month(), lastDate.Day(), lastDate.Hour(), 0, 0, 0, time.UTC)
-			aggregation := Aggregation{
-				PE5:     pE5Sum / count,
-				PE10:    PE10Sum / count,
-				PDiesel: pDieselSum / count,
-				Hour:    tempDate,
-			}
-			aggregations = append(aggregations, aggregation)
-			pE5Sum = element.PE5
-			PE10Sum = element.PE10
-			pDieselSum = element.PDiesel
-			count = 1
-			lastDate = element.Date
+			aggregation := aggregateData(dataSlice, currentHour, partition)
+			sendSingle(aggregation, client)
+			dataSlice = make([]PriceData, 1)
+			currentHour = time.Date(data.Date.Year(), data.Date.Month(), data.Date.Day(), data.Date.Hour(), 0, 0, 0, time.UTC)
 		}
 	}
-
-	return aggregations[1:]
 }
 
-func sendData(aggregations []Aggregation, client *graphigo.Client) {
-	fmt.Sprintf("Sending for region %d", aggregations[0].Region)
-	region := aggregations[0].Region
-	counter := 0
-	for _, element := range aggregations {
-		metric := graphigo.Metric{Name: fmt.Sprintf("%d.E5", element.Region), Value: element.PE5, Timestamp: element.Hour}
-		defer client.Send(metric)
-		defer client.Send(graphigo.Metric{Name: fmt.Sprintf("%d.E10", element.Region), Value: element.PE10, Timestamp: element.Hour})
-		defer client.Send(graphigo.Metric{Name: fmt.Sprintf("%d.Diesel", element.Region), Value: element.PDiesel, Timestamp: element.Hour})
-		fmt.Sprintf("Sent %d entries for region %d", counter, region)
+func aggregateData(data []PriceData, hour time.Time, region int) *Aggregation {
+	dieselSum := 0.0
+	e5Sum := 0.0
+	e10Sum := 0.0
+	count := float64(len(data))
+
+	return &Aggregation{
+		Hour:    hour,
+		Region:  region,
+		PDiesel: dieselSum / count,
+		PE10:    e10Sum / count,
+		PE5:     e5Sum / count,
 	}
 }
 
-func openConnection(partition int) *kafka.Conn {
-	conn, err := kafka.DialLeader(context.Background(), "tcp", "10.50.15.52:9092", "tankerkoenig", partition)
-	if err != nil {
-		log.Fatal("faied to dial leader:", err)
-	}
+func sendSingle(aggregation *Aggregation, client *graphigo.Client) {
+	fmt.Printf("send %v\n", *aggregation)
+	defer client.Send(graphigo.Metric{Name: fmt.Sprintf("%d.E5", aggregation.Region), Value: aggregation.PE5, Timestamp: aggregation.Hour})
+	defer client.Send(graphigo.Metric{Name: fmt.Sprintf("%d.E10", aggregation.Region), Value: aggregation.PE10, Timestamp: aggregation.Hour})
+	defer client.Send(graphigo.Metric{Name: fmt.Sprintf("%d.Diesel", aggregation.Region), Value: aggregation.PDiesel, Timestamp: aggregation.Hour})
+}
 
-	return conn
+func getReader(partition int) *kafka.Reader {
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   []string{"10.50.15.52:9092"},
+		Topic:     "tankerkoenig",
+		Partition: partition,
+		MinBytes:  10e3,
+		MaxBytes:  10e3,
+	})
+}
+
+func parseJSON(b []byte) *PriceData {
+	var data PriceData
+	json.Unmarshal(b, &data)
+	return &data
 }
